ae8fac46ea3d2706f2977903361e7b59dc841737
Frantz Maerten <fmaerten@youwol.com>
2020-12-22 15:29:16 +0100

    extended test

--- examples/simple.ts
+++ examples/simple.ts
@@ -1,20 +1,31 @@
 import {create, connect, emit} from '../src'
  
- class T {
-      constructor(private name: string) {
-          create(this, 'finished')
-          connect({sender: this, signal: 'finished', receiver: this, slot: 'done'})
-      }
-      start(sender?: T) {
-          console.log(`${this.name} is starting from sender ${sender?sender.name:'-none-'}`)
-          setTimeout( () => emit(this, 'finished', this), 500) 
-      }
-      done()  {
-          console.log(this.name, 'is done')
-      }
- }
+class T {
+    constructor(public name: string) {
+        create(this, 'finished')
+        connect({sender: this, signal: 'finished', receiver: this, slot: 'done'})
+    }
+    start(sender?: T) {
+        console.log(`${this.name} is starting from sender ${sender?sender.name:'-none-'}`)
+        setTimeout( () => emit(this, 'finished', this), 500) 
+    }
+    done()  {
+        console.log(this.name, 'is done')
+    }
+}
  
- const a = new T('A')
- const b = new T('B')
- connect({sender: a, signal: 'finished', receiver: b, slot: 'start'})
- a.start()
+const a = new T('A')
+const b = new T('B')
+connect({sender: a, signal: 'finished', receiver: b, slot: 'start'})
+connect({sender: b, signal: 'finished', receiver: (sender: T) => console.log('All done after '+sender.name)})
+a.start()
+
+/*
+Will display:
+-------------
+A is starting from sender -none-
+A is done
+B is starting from sender A
+B is done
+All done after B
+*/
