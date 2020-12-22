747c30133f42afa786b37d382193c5a2c7da03f4
Frantz Maerten <fmaerten@youwol.com>
2020-12-22 14:31:24 +0100

    simplified example

--- examples/parallel.ts
+++ examples/parallel.ts
@@ -1,21 +1,17 @@
 import {create, emit, connect} from '../src'
 
-// ----------------------------------------------
-
 class Task {
     constructor(public name: string, public ms: number) {
         create(this, 'finished')
     }
     run() {
-        console.log(`Task ${this.name} is running for ${this.ms}ms...`)
+        console.log(`${this.name} is running for ${this.ms}ms...`)
         setTimeout( () => emit(this, 'finished'), this.ms) 
     }
 }
 
-// ----------------------------------------------
-
 // Will be executed when task1 AND task2 are done
-class CombinedLatest {
+class CombineLatest {
     private doneTask1 = false
     private doneTask2 = false
 
@@ -23,38 +19,43 @@ class CombinedLatest {
         create(this, 'finished')
         connect({sender: task1, signal: 'finished', receiver: () => this.doneTask(1)})
         connect({sender: task2, signal: 'finished', receiver: () => this.doneTask(2)})
-        console.time('Total elapsed time')
     }
 
     private doneTask(n: number) {
-        console.log('done doing task', n)
         switch(n) {
             case 1: this.doneTask1 = true; break
             case 2: this.doneTask2 = true; break
         }
         if (this.doneTask1 && this.doneTask2) {
-            console.log('task 1 and 2 are done. Doing combined-latest');
+            console.log('task 1 AND 2 are done')
             this.doneTask1 = false
             this.doneTask2 = false
-            console.timeEnd('Total elapsed time')
             emit(this, 'finished')
         }
         else {
-            if (this.doneTask1) console.log('task 1 is done but still waiting for task 2');
-            else console.log('task 2 is done but still waiting for task 1')
+            const done   = this.doneTask1 ? 1 : 2
+            console.log(`task ${done} is done but still waiting for task ${3-done}`)
         }
     }
 }
 
-// ----------------------------------------------
+// Create the following:
+// ========================================
+// task1 ---\
+//           --> CombineLatest --> task3
+// rask2 ---/
+// ========================================
+//
+// task3 is launched only when task1 AND task2 are done
 
 const task1 = new Task('task 1', 3000)
 const task2 = new Task('task 2', 2000)
-const combined = new CombinedLatest(task1, task2)
+const task3 = new Task('task 3', 1000)
+const combined = new CombineLatest(task1, task2)
 
-connect({sender: combined, signal: 'finished', receiver: () => console.log('All done.')})
+connect({sender: combined, signal: 'finished', receiver: task3, slot: 'run'})
 
-// Run the 2 tasks in parallel and trigger combined
+// Run the 2 tasks in parallel and trigger combined (which will call task3)
 // when both are done
 task2.run()
 task1.run()
